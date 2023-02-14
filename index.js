ff0fc3cb02bb795949d6481355fcb40afd367a18
itscarew <itscarew@gmail.com>
2023-02-14 22:36:46 +0100

    new changes to port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import urlRoutes from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const redisPort = 6379;
 
 const app = express();
